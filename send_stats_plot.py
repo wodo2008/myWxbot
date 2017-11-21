4fc5c639335c01784a066e034c84a898f53ef8d4
liujun <liujun4717@163.com>
2017-11-21 13:03:15 +0800

    u

--- send_stats_plot.py
+++ send_stats_plot.py
@@ -10,7 +10,7 @@ import json
 
 class MyWXBot(WXBot):
     def handle_msg_all(self, msg):
-        print msg['user']['id']
+        #print msg['user']['id']
         if msg['msg_type_id'] == 4 and msg['content']['type'] == 0:
             print msg['user']['id']
             #self.send_msg_by_uid(u'hi', msg['user']['id'])
@@ -19,16 +19,7 @@ class MyWXBot(WXBot):
             #self.send_file_msg_by_uid("img/1.png", msg['user']['id'])
 
     def schedule(self):
-        groupId = ''
-        print os.path.join(os.path.split(os.path.abspath(__file__))[0],'tmp\\group_list.json')
-        group_list = json.load(open(os.path.join(os.path.split(os.path.abspath(__file__))[1],'../tmp/group_list.json')))
-        for group in group_list:
-            PYQuanPin= group['PYQuanPin']
-            UserName = group['UserName']
-            if PYQuanPin =="zhuogongshengyagongzuoshijiaoliuqun":
-                groupId = UserName
-                break
-
+        groupId = getGroupId('zhuogongshengyagongzuoshijiaoliuqun')
         canSend = True
         while True:
             if is_send_statsPlot() and canSend:
@@ -41,13 +32,24 @@ class MyWXBot(WXBot):
                 canSend = True
             time.sleep(100)
 
-
 def is_send_statsPlot():
     now = time.time()
     hour = int(time.strftime('%H',time.localtime(now)))
     if hour == 10:
         return True
     return False
+def getGroupId(pinyin):
+    groupId = ''
+    print os.path.join(os.path.split(os.path.abspath(__file__))[0],'tmp\\group_list.json')
+    group_list = json.load(open(os.path.join(os.path.split(os.path.abspath(__file__))[0],'tmp\\group_list.json')))
+    for group in group_list:
+        PYQuanPin= group['PYQuanPin']
+        UserName = group['UserName']
+        if PYQuanPin == pinyin:
+            groupId = UserName
+            break
+    return groupId
+
 
 def main():
     bot = MyWXBot()
