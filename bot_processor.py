6d00b4023cb81dfba63efa84dcea1012978d95ec
liujun <liujun4717@163.com>
2017-12-01 19:51:29 +0800

    u

--- bot_processor.py
+++ bot_processor.py
@@ -26,8 +26,8 @@ def init_redis(host,port,db,password=None):
 class MyWXBot(WXBot):
     def handle_msg_all(self, msg):
         print 'msg:',msg
-        self.group_newer_response(u'ICjiaoliuqun2datongxueba',msg)
-        self.get_send_img_members(u'ceshi',msg)
+        self.group_newer_response('ICjiaoliuqun2datongxueba',msg)
+        #self.get_send_img_members(u'ceshi',msg)
         self.auto_add_member(msg)
         self.reply_to_friends(msg)
         # if msg['msg_type_id'] == 4 and msg['content']['type'] == 0:
@@ -48,25 +48,27 @@ class MyWXBot(WXBot):
 
     #对于新加入的进行回应
     def group_newer_response(self,g_pinyin,msg):
-        print 'group_newer_response:',msg
+        print 'group_newer_response'
         if msg['user']['name'] == 'self':
             ori_group_id = msg['to_user_id']
         else:
             ori_group_id = msg['user']['id']
+        #print 'g_pinyin:',g_pinyin
+        print self.getGroupId(g_pinyin),ori_group_id
         if not ori_group_id == self.getGroupId(g_pinyin):
             print 'need:%s,this is %s' % (self.getGroupId(g_pinyin),ori_group_id)
             return
         #群文本信息
         group_invit_1 = re.compile(u'(.*?)邀请(.*?)加入了群聊')
         group_invit_2 = re.compile(u'(.*?)通过扫描(.*?)分享的二维码加入群聊')
-        if msg['msg_type_id'] == 3 and msg['content']['type'] == 0:
+        if msg['msg_type_id'] == 3 and msg['content']['type'] == 12:
             result1 = group_invit_1.findall(msg['content']['data'])
             result2 = group_invit_2.findall(msg['content']['data'])
             result = result1 if len(result1) > 0 else result2
-            print result1,result2,result
+            #print result1,result2,result
             if len(result) <= 0:
                 return
-            send_msg = '@%s欢迎加入' % (result[0][1])
+            send_msg = '@%s欢迎加入' % (eval(result[0][1]))
             self.send_msg_by_uid(send_msg,msg['user']['id'])
 
     #获取发送图片的成员
@@ -141,6 +143,7 @@ class MyWXBot(WXBot):
         groupId = ''
         print os.path.join(os.path.split(os.path.abspath(__file__))[0],'temp/group_list.json')
         group_list = json.load(open(os.path.join(os.path.split(os.path.abspath(__file__))[0],'temp/group_list.json')))
+        print 'group_list:',group_list
         for group in group_list:
             print 'group:',group
             PYQuanPin= group['PYQuanPin']
