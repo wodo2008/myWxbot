81ae80bb210e4d55634aeb39b7abc433f56da29f
wodo2008 <liujun4719@163.com>
2017-12-16 02:07:30 +0800

    u

--- bot_processor.py
+++ bot_processor.py
@@ -26,7 +26,7 @@ sys.setdefaultencoding('utf8')
 class MyWXBot(WXBot):
     def handle_msg_all(self, msg):
         print 'msg:',msg
-        self.send_kecheng_v2(msg,'IC交流群3|大同学吧')
+        self.send_kecheng_v2(msg,'IC交流群3大同学吧')
 
         #self.get_fixFriendMsg('大同学吧小助手',msg)
         # if msg['msg_type_id'] == 4 and msg['content']['type'] == 0:
@@ -42,7 +42,7 @@ class MyWXBot(WXBot):
         # t1.start()
         # t2 = threading.Thread(target=self.send_unsovled_q,args={})
         # t2.start()
-        t3 = threading.Thread(target=self.send_msg_to_group,args=('IC交流群3|大同学吧',))
+        t3 = threading.Thread(target=self.send_msg_to_group,args=('IC交流群3大同学吧',))
         t3.start()
 
     def send_kecheng_v2(self,msg,qunName):
